17ff2e87dd1f43f4e9b9845ed068c13cda4a97a7
khuongnguyen28122 <94632201+khuongnguyen28122@users.noreply.github.com>
2022-06-10 13:09:49 +0700

    khuong update

--- app/src/main/java/com/poly/onlineshopping/Fragments/GioHangFragment.java
+++ app/src/main/java/com/poly/onlineshopping/Fragments/GioHangFragment.java
@@ -3,19 +3,30 @@ package com.poly.onlineshopping.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.poly.onlineshopping.R;
 
 public class GioHangFragment extends Fragment {
+    TextView tv1,tv2;
+    Button btn1,btn2;
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_gio_hang, container, false);
+        tv1 = view.findViewById(R.id.tv_diachi);
+        tv2 = view.findViewById(R.id.tv_Tongtien);
+        btn1 = view.findViewById(R.id.btn_Thaydoi);
+        btn2 = view.findViewById(R.id.btn_Thanhtoan);
+        recyclerView = view.findViewById(R.id.rcview_GioHang);
         return view;    }
 }
--- app/src/main/java/com/poly/onlineshopping/Fragments/YeuThichFragment.java
+++ app/src/main/java/com/poly/onlineshopping/Fragments/YeuThichFragment.java
@@ -7,15 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.poly.onlineshopping.R;
 
 public class YeuThichFragment extends Fragment {
+    ImageView tru1,tru2,tru3,cong1,cong2,cong3;
+    Button btn1,btn2,btn3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_yeu_thich, container, false);
+
+        tru1 = view.findViewById(R.id.imgTru1);
+        tru2 = view.findViewById(R.id.imgTru2);
+        tru3 = view.findViewById(R.id.imgTru3);
+        cong1 = view.findViewById(R.id.imgCong1);
+        cong2 = view.findViewById(R.id.imgCong2);
+        cong3 = view.findViewById(R.id.imgCong3);
+        btn1 = view.findViewById(R.id.btn_ytThem1);
+        btn2 = view.findViewById(R.id.btn_ytThem2);
+        btn3 = view.findViewById(R.id.btn_ytThem3);
+
+
         return view;    }
 }
